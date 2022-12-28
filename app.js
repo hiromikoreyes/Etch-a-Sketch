05c50641e22a67faab43e9589b6dc273a8531c4c
hiromikoreyes <114784445+hiromikoreyes@users.noreply.github.com>
2022-12-27 21:05:32 -0500

    added a hover effect to the grid

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
+//functions
+function hoverEffect(pixel){
+    // Add the class .hover, to the pixel elements that the mouse is hovering over
+    
+    pixel.classList.add('hover');
+}
+
+
+function unHoverEffect(pixel){
+    // Remove the class .hover, to the pixel elements that the mouse is hovering over
+    
+    pixel.classList.remove('hover');
+}
+
+
+
+
 const pixelContainer = document.querySelector('#grid');
+
+
+//init 2d array
 
 let gridArray = new Array(16);
 
@@ -6,7 +26,7 @@ for (i = 0; i < 16; i ++){
     gridArray[i] = new Array(16);
 }
 
-
+//init pixel elements
 
 for (i = 0; i < 16; i ++){
 
@@ -15,5 +35,20 @@ for (i = 0; i < 16; i ++){
         gridArray[i][j].classList.add('pixel')
         pixelContainer.appendChild(gridArray[i][j]);
     }
-
 }
+
+
+//add event listeners
+
+const pixels = document.querySelectorAll('.pixel');
+
+
+/*
+Darkens the pixel that you hover over.
+*/
+pixels.forEach( pixel => {
+    pixel.addEventListener('mouseover', () => hoverEffect(pixel))
+});
+pixels.forEach( pixel => {
+    pixel.addEventListener('mouseout', () => unHoverEffect(pixel))
+});
