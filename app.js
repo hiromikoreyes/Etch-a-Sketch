ce71c1e4ad8749c35468097dfc609b5db485ff3a
hiromikoreyes <114784445+hiromikoreyes@users.noreply.github.com>
2022-12-27 21:44:03 -0500

    Created the option tocreate a grid of any size

--- app.js
+++ app.js
@@ -1,54 +1,79 @@
-//functions
+generateGrid(16,16); //default page
+
+
 function hoverEffect(pixel){
     // Add the class .hover, to the pixel elements that the mouse is hovering over
-    
     pixel.classList.add('hover');
 }
 
 
 function unHoverEffect(pixel){
     // Remove the class .hover, to the pixel elements that the mouse is hovering over
-    
     pixel.classList.remove('hover');
 }
 
 
+function generateGrid(width, height){
+    /*Generate a grid of width by height pixel elements.*/
+
+    //declare container object thingy
+    const pixelContainer = document.querySelector('#grid');
+    let gridArray = new Array(width);
 
 
-const pixelContainer = document.querySelector('#grid');
+    //change the css of the pixelContainer
+    pixelContainer.setAttribute('style',
+    `grid-template-columns: repeat(${width}, 32px);
+    grid-template-rows: repeat(${height}, 32px)`)
 
 
-//init 2d array
-
-let gridArray = new Array(16);
-
-for (i = 0; i < 16; i ++){
-    gridArray[i] = new Array(16);
-}
-
-//init pixel elements
-
-for (i = 0; i < 16; i ++){
-
-    for (j = 0; j < 16; j++){
-        gridArray[i][j] = document.createElement('div');
-        gridArray[i][j].classList.add('pixel')
-        pixelContainer.appendChild(gridArray[i][j]);
+    //init 2d array
+    for (i = 0; i < width; i ++){
+        gridArray[i] = new Array(height);
     }
+
+
+    //init pixel elements
+    for (i = 0; i < width; i ++){
+
+        for (j = 0; j < height; j++){
+            gridArray[i][j] = document.createElement('div');
+            gridArray[i][j].classList.add('pixel')
+            pixelContainer.appendChild(gridArray[i][j]);
+        }
+    }
+
+    createEventListeners();
+    
 }
 
 
-//add event listeners
+function createEventListeners(){
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach( pixel => {
+        pixel.addEventListener('mouseover', () => hoverEffect(pixel))
+    });
+    pixels.forEach( pixel => {
+        pixel.addEventListener('mouseout', () => unHoverEffect(pixel))
+    });
+}
 
-const pixels = document.querySelectorAll('.pixel');
+
+function removeAllPixels(pixelContainer){
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(pixel => {
+        pixelContainer.removeChild(pixel);
+    });
+}
 
 
-/*
-Darkens the pixel that you hover over.
-*/
-pixels.forEach( pixel => {
-    pixel.addEventListener('mouseover', () => hoverEffect(pixel))
+const btnGenerate = document.addEventListener('click', () => {
+
+    width = prompt('width','');
+    length = prompt('length','');
+
+    const pixelContainer = document.querySelector('#grid');
+    removeAllPixels(pixelContainer);
+    generateGrid(width,length);
 });
-pixels.forEach( pixel => {
-    pixel.addEventListener('mouseout', () => unHoverEffect(pixel))
-});
+
